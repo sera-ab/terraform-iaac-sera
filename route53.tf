6a6fd75b84a8ff42e23eca98264a1185e9b33548
seraaboagye <sera.aboagye@gmail.com>
2019-12-18 19:56:46 -0600

    add resources

--- route53.tf
+++ route53.tf
@@ -3,5 +3,5 @@ resource "aws_route53_record" "www" {
   name    = "www.resolversgroup.com" 
   type    = "A" 
   ttl     = "300" 
-  records = [aws_instance.web.public_ip] 
+  records = [aws_instance.web[0].public_ip,aws_instance.web[1].public_ip,aws_instance.web[2].public_ip]
 } 
