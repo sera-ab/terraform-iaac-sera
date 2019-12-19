03c74bcaa808e3a1c18390831fb363dae0e147c3
seraaboagye <sera.aboagye@gmail.com>
2019-12-18 19:07:34 -0600

    output

--- output.tf
+++ output.tf
@@ -1,3 +1,17 @@
 output "bucket_name" {
   value = aws_s3_bucket.b.bucket
 }
+
+
+output "sec_group" {
+  value = aws_security_group.allow_ssh.name
+}
+
+
+output "key_name" {
+  value = aws_key_pair.deployer.key_name
+}
+
+
+output "username" {
+  value = same_username
