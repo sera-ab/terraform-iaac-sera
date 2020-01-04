1c7c08acae25218c296d3c87b691ca55cddaf6e5
seraaboagye <sera.aboagye@gmail.com>
2020-01-04 11:37:50 -0600

    roles attached

--- IAM/iam.tf
+++ IAM/iam.tf
@@ -23,7 +23,6 @@ resource "aws_iam_policy" "policy" {
   description = "My test policy" 
   policy = <<EOF 
 { 
-
 "Version": "2012-10-17", 
 "Statement": [ 
 { 
@@ -35,4 +34,30 @@ resource "aws_iam_policy" "policy" {
 ] 
 } 
 EOF 
+} 
+
+
+resource "aws_iam_role" "test_role" { 
+name = "test_role" 
+assume_role_policy = <<EOF 
+{ 
+  "Version": "2012-10-17", 
+  "Statement": [ 
+{ 
+  "Action": "sts:AssumeRole", 
+  "Principal": { 
+  "Service": "ec2.amazonaws.com" 
+}
+  "Effect": "Allow", 
+  "Sid": "" 
+    } 
+  ]
+} 
+EOF 
+} 
+
+
+resource "aws_iam_role_policy_attachment" "test-attach" { 
+  role = aws_iam_role.test_role.name 
+  policy_arn = aws_iam_policy.policy.arn 
 } 
