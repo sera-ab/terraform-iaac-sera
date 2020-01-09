54ac51f63935dc06d8ad6ffca82c546fc94c72e3
seraaboagye <sera.aboagye@gmail.com>
2020-01-08 18:35:57 -0600

    ec2 data source

---
+++ data _source/ec2.tf
@@ -0,0 +1,15 @@
+#Go to Region us-east-2
+provider "aws" { 
+  region = "us-east-2" 
+} 
+
+#Search for Ubuntu Latest with the owner
+data "aws_ami" "ubuntu" { 
+  most_recent = true 
+  owners = ["099720109477"] 
+}
+
+# Show the AMI Id
+output "ami" { 
+  value = "${data.aws_ami.ubuntu.id}" 
+} 
