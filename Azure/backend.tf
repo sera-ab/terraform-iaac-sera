e3d0be8ddcd676c9d84f5d5d7f5a92bb18a2379d
seraaboagye <sera.aboagye@gmail.com>
2020-01-18 13:07:20 -0600

    backend

--- Azure/backend.tf
+++ Azure/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "example1"
-    storage_account_name = "example1farrukh"
+    storage_account_name = "cs2e9e15a87aadex4ea8xa28"
     container_name       = "tfstate"
     key                  = "prod_terraform.tfstate"
     access_key           = ""
