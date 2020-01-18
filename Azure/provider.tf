4f6c505daba4c7d6fdb809ededf80049e8c530e9
seraaboagye <sera.aboagye@gmail.com>
2020-01-18 11:50:41 -0600

    resource added

--- Azure/provider.tf
+++ Azure/provider.tf
@@ -2,3 +2,21 @@
 provider "azurerm" { 
    version = "1.27" 
 } 
+
+
+resource "azurerm_resource_group" "web_server_rg" { 
+  name = "web-rg" 
+  location = "westus2" 
+} 
+
+
+
+
+resource "azurerm_virtual_network" "vnet1" { 
+  name = "vnet1" 
+  location = "westus2" 
+  resource_group_name = "${azurerm_resource_group.web_server_rg.name}" 
+  address_space = ["10.0.0.0/16"] 
+} 
+
+ 
--- Azure/resourcegroup.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_resource_group" "web_server_rg" { 
-  name = "web-rg" 
-  location = "westus2" 
-
-} 
