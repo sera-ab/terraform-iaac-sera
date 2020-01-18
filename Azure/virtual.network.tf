71f41ab6cc199d002e4378b59b2123d51edcd487
seraaboagye <sera.aboagye@gmail.com>
2020-01-18 11:46:38 -0600

    update

--- Azure/virtual.network.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_virtual_network" "main" { 
-  name = "${var.prefix}-network" 
-  address_space = ["10.0.0.0/16"] 
-  location = "${azurerm_resource_group.main.location}" 
-  resource_group_name = "${azurerm_resource_group.main.name}" 
-} 
