fdd9fb6a4ba35507c25cb3521372d4bc6ebd3ae9
seraaboagye <sera.aboagye@gmail.com>
2020-01-18 11:59:14 -0600

    security groups added

--- Azure/provider.tf
+++ Azure/provider.tf
@@ -23,5 +23,24 @@ resource "azurerm_virtual_network" "vnet1" {
   resource_group_name = "${azurerm_resource_group.web_server_rg.name}" 
   virtual_network_name = "${azurerm_virtual_network.vnet1.name}" 
   address_prefix = "10.0.1.0/24" 
+} 
 
+resource "azurerm_network_security_group" "sec_group1" { 
+  name = "sec_group1" 
+  location = "${azurerm_resource_group.web_server_rg.location}" 
+  resource_group_name = "${azurerm_resource_group.web_server_rg.name}" 
+  security_rule { 
+   name = "test123" 
+   priority = 100 
+   direction = "Inbound" 
+   access = "Allow" 
+   protocol = "Tcp" 
+   source_port_range = "*" 
+   destination_port_range = "*" 
+   source_address_prefix = "*" 
+  destination_address_prefix = "*" 
+} 
+  tags = { 
+    environment = "Production" 
+  } 
 } 
